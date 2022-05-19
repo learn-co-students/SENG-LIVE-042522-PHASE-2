3651051c79dee6ec2b59d489cae5599a85961528
ixnp <enprocopios@gmail.com>
2022-05-18 18:21:22 -0700

    03 solution

--- 03-state-and-events/project_showcase/src/components/Header.js
+++ 03-state-and-events/project_showcase/src/components/Header.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Header = () => {
+const [isDarkMode, setIsDarkMode] = useState(true)
+console.log(isDarkMode)
+
+ const handleClick = () => {
+  setIsDarkMode(!isDarkMode)
+ }
+
   return (
     <header>
       <h1>
         <span className="logo">{"//"}</span>
         Project Showcase
       </h1>
-      <button>Dark Mode</button>
+      <button onClick={handleClick}>{isDarkMode?'Light Mode':'Dark Mode'}</button>
     </header>
   );
 }
